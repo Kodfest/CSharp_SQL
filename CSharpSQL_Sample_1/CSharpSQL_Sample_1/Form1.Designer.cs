c7eab300f16ac916fc2fd4a3dbccc0ffafc03c96
Yavuz Gedik <yavuzgedik@outlook.com>
2016-05-22 16:36:32 +0300

    Insert And Select Query

--- CSharpSQL_Sample_1/CSharpSQL_Sample_1/Form1.Designer.cs
+++ CSharpSQL_Sample_1/CSharpSQL_Sample_1/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LastNameTxtBox = new System.Windows.Forms.TextBox();
+            this.FirstNameTxtBox = new System.Windows.Forms.TextBox();
+            this.DataListBox = new System.Windows.Forms.ListBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.readBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "LastName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "FirstName";
+            // 
+            // LastNameTxtBox
+            // 
+            this.LastNameTxtBox.Location = new System.Drawing.Point(160, 41);
+            this.LastNameTxtBox.Name = "LastNameTxtBox";
+            this.LastNameTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.LastNameTxtBox.TabIndex = 18;
+            // 
+            // FirstNameTxtBox
+            // 
+            this.FirstNameTxtBox.Location = new System.Drawing.Point(160, 15);
+            this.FirstNameTxtBox.Name = "FirstNameTxtBox";
+            this.FirstNameTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.FirstNameTxtBox.TabIndex = 17;
+            // 
+            // DataListBox
+            // 
+            this.DataListBox.FormattingEnabled = true;
+            this.DataListBox.Location = new System.Drawing.Point(276, 12);
+            this.DataListBox.Name = "DataListBox";
+            this.DataListBox.Size = new System.Drawing.Size(120, 95);
+            this.DataListBox.TabIndex = 16;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(12, 41);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 15;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // readBtn
+            // 
+            this.readBtn.Location = new System.Drawing.Point(12, 12);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(75, 23);
+            this.readBtn.TabIndex = 14;
+            this.readBtn.Text = "Read";
+            this.readBtn.UseVisualStyleBackColor = true;
+            this.readBtn.Click += new System.EventHandler(this.readBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 259);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LastNameTxtBox);
+            this.Controls.Add(this.FirstNameTxtBox);
+            this.Controls.Add(this.DataListBox);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.readBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LastNameTxtBox;
+        private System.Windows.Forms.TextBox FirstNameTxtBox;
+        private System.Windows.Forms.ListBox DataListBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button readBtn;
     }
 }
 
--- CSharpSQL_Sample_1/CSharpSQL_Sample_1/Form1.cs
+++ CSharpSQL_Sample_1/CSharpSQL_Sample_1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,79 @@ namespace CSharpSQL_Sample_1
 {
     public partial class Form1 : Form
     {
+        SqlConnection connect; //Sql bağlantı nesnemiz
+        SqlCommand cmd; //Sql sorgu nesnemiz
+
+        //Connection String
+        string conString = @"Server=tcp:kodfest-server.database.windows.net,1433;
+            Data Source=kodfest-server.database.windows.net;Initial Catalog=kodfest;
+            Persist Security Info=False;User ID={user_name};Password={user_password};
+            Pooling=False;MultipleActiveResultSets=False;Encrypt=True;
+            TrustServerCertificate=False;Connection Timeout=30;";
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void readBtn_Click(object sender, EventArgs e)
+        {
+            /*
+            Her okuma işleminde verilerin tekrar yazılmasını 
+            engelllemek için listbox temizleniyor.
+            */
+            DataListBox.Items.Clear();
+
+            //connect objesine ConnectionString parametreli atama
+            connect = new SqlConnection(conString);
+
+            connect.Open(); //Veri tabanı bağlantısı başlatılıyor
+
+            /*
+            Persons tablosundaki tüm kayıtların FirstName bilgilerini 
+            getiren sorguyu cmd nesnesine atama
+            */
+            cmd = new SqlCommand("select FirstName from Persons", connect);
+
+            /* 
+            Veri okuma işlemleri için kullanılan
+            .sqlDataReader tipinde PersonsReader objesine
+            okuma yapacak işlemi atama
+            */
+            SqlDataReader PersonsReader = cmd.ExecuteReader();
+
+            /*
+            PersonsReade'dan okuma yapıldıkça
+            her kaydın FirstName değeri DataListBox'a eklenir 
+            */
+            while (PersonsReader.Read())
+            {
+                DataListBox.Items.Add(PersonsReader["FirstName"].ToString());
+            }
+
+            //İşlemler sona erince bağlantı kapatılır.
+
+            connect.Dispose(); // veya connect.Close();
+        }
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            connect = new SqlConnection(conString);
+
+            connect.Open();
+
+            //Person ekleme sorgusu
+            cmd = new SqlCommand
+                ("INSERT INTO Persons (FirstName,LastName) VALUES (@FirstName, @LastName)"
+                , connect);
+
+            //FirstName Parametresi texbox'dan alınıyor.
+            cmd.Parameters.AddWithValue("@FirstName", FirstNameTxtBox.Text);
+
+            //LasstName Parametresi texbox'dan alınıyor.
+            cmd.Parameters.AddWithValue("@LastName", LastNameTxtBox.Text);
+
+            cmd.ExecuteNonQuery(); //sorgu çalıştırılıyor
+
+            connect.Dispose();
         }
     }
 }
