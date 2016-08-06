eccec2b8886fe5dc5dfc08d787fa6a9268b0942d
yavuzgedik <yavuzgedik@outlook.com>
2016-08-06 17:21:33 +0100

    Updated

--- CSharpSQL_Sample_2/CSharpSQL_Sample_2/Form1.Designer.cs
+++ CSharpSQL_Sample_2/CSharpSQL_Sample_2/Form1.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.singinBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTbox = new System.Windows.Forms.TextBox();
+            this.signupBtn = new System.Windows.Forms.Button();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.userNameTbox = new System.Windows.Forms.TextBox();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(80, 159);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(122, 23);
+            this.deleteBtn.TabIndex = 15;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // singinBtn
+            // 
+            this.singinBtn.Location = new System.Drawing.Point(143, 72);
+            this.singinBtn.Name = "singinBtn";
+            this.singinBtn.Size = new System.Drawing.Size(58, 23);
+            this.singinBtn.TabIndex = 14;
+            this.singinBtn.Text = "Sign In";
+            this.singinBtn.UseVisualStyleBackColor = true;
+            this.singinBtn.Click += new System.EventHandler(this.singinBtn_Click);
+            // 
+            // editBtn
+            // 
+            this.editBtn.Location = new System.Drawing.Point(79, 130);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(122, 23);
+            this.editBtn.TabIndex = 13;
+            this.editBtn.Text = "Edit";
+            this.editBtn.UseVisualStyleBackColor = true;
+            this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(10, 47);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(56, 13);
+            this.passwordLabel.TabIndex = 12;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // passwordTbox
+            // 
+            this.passwordTbox.Location = new System.Drawing.Point(79, 44);
+            this.passwordTbox.Name = "passwordTbox";
+            this.passwordTbox.Size = new System.Drawing.Size(122, 20);
+            this.passwordTbox.TabIndex = 11;
+            // 
+            // signupBtn
+            // 
+            this.signupBtn.Location = new System.Drawing.Point(79, 72);
+            this.signupBtn.Name = "signupBtn";
+            this.signupBtn.Size = new System.Drawing.Size(58, 23);
+            this.signupBtn.TabIndex = 10;
+            this.signupBtn.Text = "Sign Up";
+            this.signupBtn.UseVisualStyleBackColor = true;
+            this.signupBtn.Click += new System.EventHandler(this.signupBtn_Click);
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(10, 18);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(63, 13);
+            this.userNameLabel.TabIndex = 9;
+            this.userNameLabel.Text = "User Name:";
+            // 
+            // userNameTbox
+            // 
+            this.userNameTbox.Location = new System.Drawing.Point(79, 15);
+            this.userNameTbox.Name = "userNameTbox";
+            this.userNameTbox.Size = new System.Drawing.Size(122, 20);
+            this.userNameTbox.TabIndex = 8;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(79, 101);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(122, 23);
+            this.exitBtn.TabIndex = 16;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.singinBtn);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.passwordTbox);
+            this.Controls.Add(this.signupBtn);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.userNameTbox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button singinBtn;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordTbox;
+        private System.Windows.Forms.Button signupBtn;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.TextBox userNameTbox;
+        private System.Windows.Forms.Button exitBtn;
     }
 }
 
--- CSharpSQL_Sample_2/CSharpSQL_Sample_2/Form1.cs
+++ CSharpSQL_Sample_2/CSharpSQL_Sample_2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,156 @@ namespace CSharpSQL_Sample_2
 {
     public partial class Form1 : Form
     {
+        // LOCAL DB Connection String https://www.connectionstrings.com/sql-server/
+        SqlConnection cs = new SqlConnection("Server=WINDOWS10\\SQLEXPRESS;Initial Catalog=Deneme;Integrated Security=true;");
+        SqlCommand cmd;
         public Form1()
         {
             InitializeComponent();
+
+            //Buttons Visibility
+            editBtn.Visible = false;
+            deleteBtn.Visible = false;
+        }
+        private void signupBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(userNameTbox.Text) || string.IsNullOrWhiteSpace(passwordTbox.Text))
+            {
+                MessageBox.Show("Cannot be empty!");
+            }
+            else
+            {
+                cs.Open();
+                cmd = new SqlCommand("SELECT * FROM Person WHERE UserName = '" + userNameTbox.Text + "'", cs);
+                var rdr = cmd.ExecuteScalar();
+                cs.Close();
+                if (rdr != null)
+                {
+                    MessageBox.Show("Username is already in use. Please choose another one!");
+                }
+                else
+                {
+                    try
+                    {
+                        cs.Open();
+                        cmd = new SqlCommand("INSERT INTO Person (UserName, Password, RegisterDate) VALUES (@UserName, @Password, @RegisterDate)", cs);
+                        cmd.Parameters.AddWithValue("@UserName", userNameTbox.Text);
+                        cmd.Parameters.AddWithValue("@Password", passwordTbox.Text);
+                        cmd.Parameters.AddWithValue("@RegisterDate", DateTime.Now.ToString());
+
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Successfully!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        cs.Close();
+                    }
+                }
+            }
+        }
+
+        private void singinBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(userNameTbox.Text) || string.IsNullOrWhiteSpace(passwordTbox.Text))
+            {
+                MessageBox.Show("Cannot be empty!");
+            }
+            else
+            {
+                cs.Open();
+                cmd = new SqlCommand("SELECT * FROM Person WHERE UserName = '" + userNameTbox.Text + "' AND Password = '" + passwordTbox.Text + "'", cs);
+                var rdr = cmd.ExecuteScalar();
+
+                if (rdr == null)
+                {
+                    MessageBox.Show("User not found!");
+                }
+                else
+                {
+                    editBtn.Visible = true;
+                    deleteBtn.Visible = true;
+                }
+                cs.Close();
+            }
+        }
+
+        private void editBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(userNameTbox.Text) || string.IsNullOrWhiteSpace(passwordTbox.Text))
+            {
+                MessageBox.Show("Cannot be empty!");
+            }
+            else
+            {
+                try
+                {
+                    cs.Open();
+                    cmd = new SqlCommand("UPDATE Person SET (UserName = @UserName) AND (Password = @Password) WHERE (UserName = @UserName)", cs);
+                    cmd.Parameters.AddWithValue("@UserName", userNameTbox.Text);
+                    cmd.Parameters.AddWithValue("@Password", passwordTbox.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Successfully!");
+                    editBtn.Visible = false;
+                    deleteBtn.Visible = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    cs.Close();
+                }
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(userNameTbox.Text) || string.IsNullOrWhiteSpace(passwordTbox.Text))
+            {
+                MessageBox.Show("Cannot be empty!");
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(userNameTbox.Text) || string.IsNullOrWhiteSpace(passwordTbox.Text))
+                {
+                    MessageBox.Show("Cannot be empty!");
+                }
+                else
+                {
+                    try
+                    {
+                        cs.Open();
+                        cmd = new SqlCommand("DELETE FROM Person WHERE (UserName = @UserName)", cs);
+                        cmd.Parameters.AddWithValue("@UserName", userNameTbox.Text);
+                        cmd.Parameters.AddWithValue("@Password", passwordTbox.Text);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Successfully!");
+                        editBtn.Visible = false;
+                        deleteBtn.Visible = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        cs.Close();
+                    }
+                }
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            editBtn.Visible = false;
+            deleteBtn.Visible = false;
+            userNameTbox.Clear();
+            passwordTbox.Clear();
         }
     }
 }
